e7349259bc8331314930e1c2ee9fc69eb1f1671e
divyesh-gehlot <gehlotdivyeshsingh@gmail.com>
2024-12-28 12:41:23 +0530

    Test Run

--- SupraNation/src/Sections/Hero.jsx
+++ SupraNation/src/Sections/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
     <>
       <div>
         <div>
-          <video src={videoBg} filter autoPlay loop muted className='h-96 object-cover w-full brightness-50 md:w-full md:h-full md:object-cover md:brightness-50 lg:object-cover lg:brightness-50'></video>
+          <video src={videoBg} filter autoPlay loop className='h-96 object-cover w-full brightness-50 md:w-full md:h-full md:object-cover md:brightness-50 lg:object-cover lg:brightness-50'></video>
           <div className='absolute top-48 left-5 md:absolute md:bottom-44 md:left-10 lg:absolute lg:top-44 lg:left-10'>
             <div className='mb-2 text-2xl w-full font-mono md:mb-10 md:text-6xl md:w-1/3 md:font-mono lg:mb-10 lg:text-6xl lg:w-1/3 lg:font-mono'><h1>Welcome to <span className='md:text-8xl font-extrabold'>Supra<span className='text-red-700'>Nation</span></span></h1></div>
             <div className='mb-2 text-sm w-full font-mono md:mb-10 md:text-2xl md:w-1/3 md:font-mono lg:mb-10 lg:text-2xl lg:w-1/3 lg:font-mono'><p>Experience the thrill of speed and precision with the legendary Toyota Supra. At SupraFever, explore, discover, and fuel your passion for this iconic masterpiece.</p></div>
